7b799fbce5adc35974bdca256e563c3e09f11f1a
mihkhoi <minhkhoi532009@gmail.com>
2023-01-07 13:11:06 +0700

    Create bai29.cpp

---
+++ bai29.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+void nhap(int a[], int n)
+{
+       for(int i = 0; i < n;i++)
+       {
+              cout << "a["<<i<<"]= ";
+              cin >> a[i];
+       }
+}
+int tc(int a[], int n)
+{
+       int s = 0;
+       for(int i = 0;i < n;i++)
+       {
+              if(a[i]%2 == 0)
+              {
+                     s = s + a[i];
+              }
+       }
+       return s;
+}
+
+void xuat(int a[], int n)
+{
+       for(int i = 0;i < n;i++)
+       {
+              cout << a[i] << " ";
+       }
+}
+
+int main()
+{
+       int a[100], n;
+       cin >> n;
+       nhap(a,n);
+       xuat(a,n);
+       cout << endl << "Tong cac so chan= " << tc(a,n);
+       return 0;
+}
