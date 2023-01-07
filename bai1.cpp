35bb69a93158e6fd7af18b5f19ae109a75093127
mihkhoi <minhkhoi532009@gmail.com>
2023-01-07 13:07:40 +0700

    Create bai1.cpp

---
+++ bai1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+       int a, s, p;
+       cout << "Nhap vao canh hinh vuong: ";
+       cin >> a;
+       s = a*a;
+       p = a*4;
+       cout << "Chu vi hinh vuong = " << p << endl;
+       cout << "Dien tich hinh vuong = " << s;
+       return 0; 
+}
