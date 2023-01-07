7bc53b0855d107ff38f9e20cd616d9724c99399e
mihkhoi <minhkhoi532009@gmail.com>
2023-01-07 13:11:13 +0700

    Create bai31.cpp

---
+++ bai31.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+       int n;
+       cin >> n;
+       if(n%2 == 0)
+       {
+              cout << n << " la so chan";
+       }
+       else
+       {
+              cout << n << " la so le";
+       }
+       return 0;
+}
