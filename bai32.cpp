3342127cdf208d0a1f0ab463fbc78dd687369a7d
mihkhoi <minhkhoi532009@gmail.com>
2023-01-07 13:10:52 +0700

    Create bai32.cpp

---
+++ bai32.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+       float n;
+       cout << "Nhap diem trung binh mon Nhap mon lap trinh: ";
+       cin >> n;
+       if(n >= 4.0)
+       {
+              cout << "Dau";
+       }
+       else
+       {
+              cout << "Rot";
+       }
+       return 0;
+}
