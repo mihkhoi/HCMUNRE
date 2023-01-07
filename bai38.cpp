6036c402c925e0a98e4f8f0ec7c3bc8cf620dd05
mihkhoi <minhkhoi532009@gmail.com>
2023-01-07 13:11:28 +0700

    Create bai38.cpp

---
+++ bai38.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+       int n;
+       cout << "Nhap tuoi nam nay cua ban: ";
+       cin >> n;
+       cout << "Nam sinh cua ban tinh theo nam nay: " << 2022 - n;
+       return 0;
+}
