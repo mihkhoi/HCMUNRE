a9cebb12f42fcba83c9bb1d61ee50a8279a2eaf1
mihkhoi <minhkhoi532009@gmail.com>
2023-01-07 13:12:06 +0700

    Create bai69.cpp

---
+++ bai69.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+#define MAX 100
+
+void nhap(int a[], int n)
+{
+       for(int i = 0;i < n;i++)
+       {
+              cout << "a["<<i<<"]= ";
+              cin >> a[i];
+       }
+}
+
+void xuat(int a[], int n)
+{
+       cout << "Mang vua nhap: ";
+       for(int i = 0;i < n;i++)
+       {
+              cout << a[i] << " ";
+       }
+}
+
+int main()
+{
+       int a[MAX], n;
+       cout << "Nhap so phan tu cua mang: ";
+       cin >> n;
+       nhap(a,n);
+       xuat(a,n);
+       return 0;
+}
