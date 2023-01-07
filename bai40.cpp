4d128ffc980a90fde7b9ea163a042553ed69ddac
mihkhoi <minhkhoi532009@gmail.com>
2023-01-07 13:12:38 +0700

    Create bai40.cpp

---
+++ bai40.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+       int a, b;
+       cin >> a >> b;
+       if(a%b == 0)
+       {
+              cout << "a la boi cua b";
+       }
+       else
+       {
+              cout << "a khong phai la boi cua b";
+       }
+       return 0;
+}
