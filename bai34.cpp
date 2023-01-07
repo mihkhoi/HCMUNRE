a701ffbd97a85c0be4aefd92ad4bb46793756ea7
mihkhoi <minhkhoi532009@gmail.com>
2023-01-07 13:10:57 +0700

    Create bai34.cpp

---
+++ bai34.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+       float r, p, s;
+       cin >> r;
+       p = (r*2)*3.14;
+       s = r*r*3.14;
+       cout << p << endl;
+       cout << s << endl;
+       return 0;
+}
