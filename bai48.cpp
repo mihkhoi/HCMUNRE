f8dca33e1a6cb9ce71e4636e32f6970d72884f2c
mihkhoi <minhkhoi532009@gmail.com>
2023-01-07 13:13:34 +0700

    Create bai48.cpp

---
+++ bai48.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+       int x;
+       double f;
+       cin >> x;
+       f = (3*pow(x,2) + 4*x + 5) / (2*x + 1);
+       cout << f;
+}
